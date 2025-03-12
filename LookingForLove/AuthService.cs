1484b7c66d1abc53d1036a27ea5c8f05f5c9cc0e
Logan Mccallum <l_mccallum192676@fanshaweonline.ca>
2025-03-12 00:02:09 -0400

    Add VERY simple login will 100% need to change down the line

---
+++ LookingForLove/AuthService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace LookingForLove
+{
+    public class AuthService
+    {
+        private const string FilePath = "users.json";
+
+        public void Register(string username, string password)
+        {
+            List<User> users = LoadUsers();
+            if (users.Exists(u => u.Username == username))
+            {
+                Console.WriteLine("User already exists!");
+                return;
+            }
+
+            users.Add(new User { Username = username, Password = password });
+            SaveUsers(users);
+            Console.WriteLine("User registered successfully!");
+        }
+
+        public bool Login(string username, string password)
+        {
+            List<User> users = LoadUsers();
+            return users.Exists(u => u.Username == username && u.Password == password);
+        }
+
+        private List<User> LoadUsers()
+        {
+            if (!File.Exists(FilePath))
+                return new List<User>();
+
+            string json = File.ReadAllText(FilePath);
+            return JsonConvert.DeserializeObject<List<User>>(json) ?? new List<User>();
+        }
+
+        private void SaveUsers(List<User> users)
+        {
+            string json = JsonConvert.SerializeObject(users, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(FilePath, json);
+        }
+    }
+}
--- LookingForLove/Program.cs
+++ LookingForLove/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LookingForLove;
+
+class Program
+{
+    static void Main()
+    {
+        AuthService authService = new AuthService();
+
+        Console.WriteLine("1. Register");
+        Console.WriteLine("2. Login");
+        Console.Write("Select an option: ");
+        string option = Console.ReadLine();
+
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        if (option == "1")
+        {
+            authService.Register(username, password);
+        }
+        else if (option == "2")
+        {
+            if (authService.Login(username, password))
+            {
+                Console.WriteLine("Login successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid option.");
+        }
+    }
+}
